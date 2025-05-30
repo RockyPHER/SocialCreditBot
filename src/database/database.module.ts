2fcbf9be14a66856b57ce40307c5366659e0e46d
Robot <robotkarasu@proton.me>
2025-05-30 00:36:09 -0400

    feat: :art: adicionado Dbmodule para global

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
   providers: [UsersService, PrismaService],
   exports: [UsersService, PrismaService],
